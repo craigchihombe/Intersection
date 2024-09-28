62bd569764b75c552193df2a1a62ac14987e5cae
craigchihombe <achihombe@gmail.com>
2024-09-28 10:27:24 +0200

    Add files via upload

---
+++ Intersection.py
@@ -0,0 +1,8 @@
+setx = {"Green","Blue"}
+sety = {"Blue","Yellow"}
+print("Original set elements are:")
+print(setx)
+print(sety)
+print("\nintersections of two sets are:")
+setz = setx.intersection(sety)
+print(setz)
